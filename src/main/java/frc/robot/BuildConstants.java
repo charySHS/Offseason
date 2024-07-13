e6d9a3f69fe6d6bf7d731149d5320f559a93c0ab
chary <smkzachatac@gmail.com>
2024-07-13 13:43:11 -0700

    Fixed build.gradle with working status

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Offseason";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 2;
+  public static final String GIT_SHA = "931d9c78fc2d381105bc0eb280caf46c66c0fbad";
+  public static final String GIT_DATE = "2024-07-13 13:26:24 PDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-07-13 13:41:19 PDT";
+  public static final long BUILD_UNIX_TIME = 1720903279371L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
